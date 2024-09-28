9f6e692b0d84d5ccc0d24271f55ba4d572c05d33
Alex Thompson <alex.thompson@spinmesolutions.com>
2024-09-28 19:53:01 +0100

    init

---
+++ webserver/target/debug/deps/webserver-bfcdc1d03292a436.d
@@ -0,0 +1,5 @@
+C:\Users\Blunt\Desktop\rust\kitchen-sink\webserver\target\debug\deps\libwebserver-bfcdc1d03292a436.rmeta: src\main.rs
+
+C:\Users\Blunt\Desktop\rust\kitchen-sink\webserver\target\debug\deps\webserver-bfcdc1d03292a436.d: src\main.rs
+
+src\main.rs:
---
+++ webserver/target/debug/deps/webserver-ca0eeeaab1025a17.d
@@ -0,0 +1,5 @@
+C:\Users\Blunt\Desktop\rust\kitchen-sink\webserver\target\debug\deps\libwebserver-ca0eeeaab1025a17.rmeta: src\main.rs
+
+C:\Users\Blunt\Desktop\rust\kitchen-sink\webserver\target\debug\deps\webserver-ca0eeeaab1025a17.d: src\main.rs
+
+src\main.rs:
